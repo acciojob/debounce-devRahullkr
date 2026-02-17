865a527754ba5bc5d604ba27d8f81f702f5d944d
Rahul Kr <rahulkumar8604745@gmail.com>
2026-02-17 16:20:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function debounce(callback, delay, immediate = false) {
+
+	let timeoutId = null
+
+	return function debounced(...args) {
+		const context = this
+		const shouldCallImmediate = immediate && timeoutId === null;
+
+
+		if(timeoutId !== null){
+			clearTimeout(timeoutId)
+		}
+
+		timeoutId = setTimeout(() => {
+			timeoutId = null
+		},delay)
+	}
   
   }
   
